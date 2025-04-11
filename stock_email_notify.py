3a2d3bbb60faa7e143e951d0da33a062b60e5518
jin11109 <104644148+jin11109@users.noreply.github.com>
2025-04-11 12:31:36 +0800

    Update html request by Flask

--- stock_email_notify.py
+++ stock_email_notify.py
@@ -1,19 +1,29 @@
 import os
+from flask import Flask, request
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-api_key = os.environ['SENDGRID_API_KEY']
-from_email = os.environ['FROM_EMAIL']
-to_emails = os.environ['TO_EMAILS']
+app = Flask(__name__)
 
-message = Mail(
-    from_email=from_email,
-    to_emails=to_emails,
-    subject='test',
-    html_content='<strong>with render</strong>')
+@app.route('/send-email', methods=['GET'])
+def send_mail():
+    try:
+        api_key = os.environ['SENDGRID_API_KEY']
+        from_email = os.environ['FROM_EMAIL']
+        to_emails = os.environ['TO_EMAILS']
 
-sg = SendGridAPIClient(api_key)
-response = sg.send(message)
-print(response.status_code)
-print(response.body)
-print(response.headers)
+        message = Mail(
+            from_email=from_email,
+            to_emails=to_emails,
+            subject='test',
+            html_content='<strong>with render</strong>')
+
+        sg = SendGridAPIClient(api_key)
+        response = sg.send(message)
+        return f'Email sent: {response.status_code}', 20
+        
+    except Exception as e:
+        return f'Error: {str(e)}', 500
+
+if __name__ == '__main__':
+    app.run()
